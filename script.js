f898020afb6d0ab7902dba44cb1bca72321e911d
Mark-a-obrien <mark.a.obrien00@gmail.com>
2023-05-19 12:59:48 +0100

    Added timer and game end message

--- script.js
+++ script.js
@@ -2,36 +2,66 @@
 const main = document.querySelector(".main");
 const holes = document.querySelectorAll(".hole");
 const pointsElement = document.querySelector("#points");
+const highScoreElement = document.querySelector("#high-score");
 const difficultySlider = document.querySelector("#difficultySlider");
 const difficulty = document.querySelector("#difficulty");
+const message = document.querySelector("#message");
+const timerElement = document.querySelector("#timer");
+const playAgain = document.querySelector("#play-again");
+const timerSlider = document.querySelector("#timerSlider");
+
 
 const moleSRC = "images/mole.png";
 const holeSRC = "images/hole.png";
 
+let points = 0;
+let highScore = 0;
+let difficultyValue = Math.floor(difficultySlider.value / 10 / 4 + 1);
+let timerLength = Math.floor(timerSlider.value / 2);
+let timerOn = false;
+
+timerElement.textContent = timerLength;
+
 holesList = [];
 
 spawnMole();
-displayDifficulty()
-
-let points = 0;
-let difficultyValue = Math.floor(difficultySlider.value / 10 / 4 + 1);
+displayDifficulty();
+displayTimerValue();
 
 
 holes.forEach((hole) => {
     
     const holeSRC = hole.src;
 
-    console.log(holeSRC);
+    // console.log(holeSRC);
 
     hole.addEventListener("click", () => {
 
-        console.log(hole.src);
+        // console.log(hole.src);
         difficultyValue = Math.floor(difficultySlider.value / 10 / 4 + 1);
+        timerLength = Math.floor(timerSlider.value / 2);
 
         if (hole.src != holeSRC) {
+
+            // disable timer slider. so user cant change time mid game
+            timerSlider.disabled = true;
+
+
+            
+            
             hole.src = holeSRC;
             setDifficulty(difficultyValue);
             addPoint();
+
+            if (!timerOn) {
+                timer(timerLength); // Starts the timer
+                timerOn = true;
+            }
+
+            // if (points >= 10) {
+            //     showMessage();
+            // }
+            
         }
     });
 })
@@ -40,22 +70,23 @@ function wait(x) {
     return new Promise((resolve) => {
         setTimeout(() => {
         resolve(x);
-        }, 1000);
+        }, x);
     });
 }
 
 async function spawnMole() {
-    const x = await wait(10);
+    const x = await wait(1000);
 
     const moleID = getRandomInt(0, 9);
-    console.log(moleID);
+    // console.log(moleID);
     holes[moleID].src = moleSRC;
 }
 
+// Gets a random number between max & min
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (max - min) + min); 
 }
   
 
@@ -73,9 +104,68 @@ function displayDifficulty() {
     difficulty.textContent = Math.floor(difficultySlider.value / 10 / 4 + 1);
 }
 
+
 function setDifficulty(difficulty) {
     for (let i = 0; i < difficulty; i++) {
-        console.log(i);
+        // console.log(i);
         spawnMole();
     }
 }
+
+// displays message
+function showMessage() {
+    main.style = "display : none;";
+    message.style = "display : block;"
+    message.children[0].textContent = `Well done! You scored ${points} points!`
+
+    // await wait(1000);
+    // message.style = "display : none;"
+}
+
+async function timer(time) {
+    let startTime = time;
+    for (let i = 0; i < startTime; i++) {
+        await wait(1000);
+        time -= 1;
+        timerElement.textContent = time;
+        console.log(time);
+    }
+    showMessage();
+}
+
+
+
+function restartGame() {
+    highScore = points;
+    highScoreElement.textContent = points;
+    points = 0;
+    pointsElement.textContent = points;
+    difficultyValue = Math.floor(difficultySlider.value / 10 / 4 + 1);
+    timerLength = 2;
+    timerOn = false;
+
+    timerSlider.disabled = false; // renable timer slider
+    timerElement.textContent = timerLength;
+
+    holes.forEach((hole) => {
+        hole.src = holeSRC;
+    });
+
+    spawnMole();
+}
+
+
+playAgain.addEventListener("click", () => {
+    restartGame();
+    main.style = "display : flex;";
+    message.style = "display : none;"
+});
+
+
+function displayTimerValue() { 
+    timerElement.textContent = Math.floor(timerSlider.value / 2);
+}
+
+timerSlider.oninput = () => {
+    displayTimerValue();
+};
